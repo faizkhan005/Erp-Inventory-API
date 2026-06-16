c6d6c5b4ba3feda34dcc44336bfae671a38eb776
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-06-16 17:01:56 -0400

    Added Correlation Middileware

---
+++ ErpInventoryApi/ERPInventoryApi.API/Middleware/CorrelationIdMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace ERPInventoryApi.API.Middleware;
+
+public class CorrelationIdMiddleware
+{
+    private const string CorrelationIdHeader = "X-Correlation-Id";
+    private readonly RequestDelegate _next;
+    private readonly ILogger<CorrelationIdMiddleware> _logger;
+
+    public CorrelationIdMiddleware(RequestDelegate next, ILogger<CorrelationIdMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        // Use caller-supplied ID if present, otherwise generate one
+        var correlationId = context.Request.Headers[CorrelationIdHeader].FirstOrDefault()
+            ?? Guid.NewGuid().ToString();
+
+        // Make it available to downstream middleware and controllers
+        context.Items[CorrelationIdHeader] = correlationId;
+        context.Response.OnStarting(() =>
+        {
+            context.Response.Headers[CorrelationIdHeader] = correlationId;
+            return Task.CompletedTask;
+        });
+
+        using (_logger.BeginScope(new Dictionary<string, object>
+        { [CorrelationIdHeader] = correlationId }))
+        {
+            await _next(context);
+        }
+    }
+}
