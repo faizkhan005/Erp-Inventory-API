7b50f234abd60aed2ad9616b1a867065df86bce7
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-06-16 14:19:17 -0400

    Services Registered In program.cs Required Nuget Packages Added DTO for auth completed

---
+++ ErpInventoryApi/ERPInventoryApi.Application/DTOs/Auth/RegisterRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace ERPInventoryApi.Application.DTOs.Auth;
+
+public record RegisterRequest(string Username, string Password, string Role = "User");
---
+++ ErpInventoryApi/ERPInventoryApi.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ERPInventoryApi.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "User"; // "Admin" | "User"
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
