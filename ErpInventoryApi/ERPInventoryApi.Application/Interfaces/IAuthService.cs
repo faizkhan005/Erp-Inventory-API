27b2ad42218869900210a90c62fbd32a741f07a0
Faizan Ahmed Khan R K <faizanahmedkhan005@gmail.com>
2026-06-16 14:27:17 -0400

    Auth Service Created

---
+++ ErpInventoryApi/ERPInventoryApi.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using ERPInventoryApi.Application.DTOs.Auth;
+
+namespace ERPInventoryApi.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<AuthResponse> RegisterAsync(RegisterRequest request);
+    Task<AuthResponse> LoginAsync(LoginRequest request);
+}
